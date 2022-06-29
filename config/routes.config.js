27c3f592005875052464ed0e4fd64d3134d56bf8
Marc Cuesta <marc.cuestacarabasa@gmail.com>
2022-06-29 21:33:27 +0200

    Iterations 1-2-3 done, iteration 4 started

--- config/routes.config.js
+++ config/routes.config.js
@@ -1,11 +1,11 @@
+// TODO
 const express = require("express");
 const router = express.Router();
 
-const misc = require("../controllers/misc.controller");
-const spotify = require("../controllers/spotify.controller");
+const spotify = require("../controllers/spotify.controller")
 
-
-router.get("/", misc.home);
-router.get("/artist-search", spotify.searchArtists)
+router.get("/", spotify.home);
+router.get("/artist-search", spotify.search);
+router.get("/albums/:artistId", spotify.albums);
 
 module.exports = router;
--- controllers/spotify.controller.js
+++ controllers/spotify.controller.js
@@ -1,14 +1,31 @@
-const SpotifyWebApi = require("spotify-web-api-node");
-
 // TODO
-module.exports.searchArtists = (req, res, next) => {
+const spotifyApi = require("../config/spotify.config");
 
-    SpotifyWebApi.searchArtists(req.query.search)
-  .then((data) => {
-    console.log("The received data from the API: ", data.body);
-    res.render("", { artists: data.body })
-  })
-  .catch((err) =>
-    console.log("The error while searching artists occurred: ", err)
-  );
+//Module.exports prepara el código que metamos después del "=" para ser aprovechado en otro scope con "require"
+module.exports.home = (req, res) => {
+    res.render("home") //no indicamos ruta "/views/home" porque al instalar handlebars los archivos .hbs se entiende que van por defecto en la carpeta views
+}
+
+module.exports.search = (req, res) => {
+    spotifyApi
+        .searchArtists(req.query.search)
+        .then((data) => {
+            //return res.json(data) --> podemos usar para pintar el json resultante en el navegador, corta el flujo del código por hacer return
+            //console.log("The received data from the API: ", data.body); --> lo mismo que la línea anterior pero nos lo saca por consola sin cortar el flujo
+            res.render("artist-search-results", { artists: data.body.artists.items })
+        })
+        .catch((err) =>
+            console.log("The error while searching artists occurred: ", err)
+        );
+}
+
+module.exports.albums = (req, res) => {
+    spotifyApi
+        .getArtistAlbums(req.params.artistId)
+        .then((data) => {
+            res.render("albums", { albums: data.body.items })
+        })
+        .catch((err) =>
+            console.log("The error while searching artists occurred: ", err)
+        );
 }
