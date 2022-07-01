544bf218bc059871ec12390d1f238a68d1ddec50
Marc Cuesta <marc.cuestacarabasa@gmail.com>
2022-07-01 19:43:00 +0200

    Lab solved

--- config/routes.config.js
+++ config/routes.config.js
@@ -7,5 +7,6 @@ const spotify = require("../controllers/spotify.controller")
 router.get("/", spotify.home);
 router.get("/artist-search", spotify.search);
 router.get("/albums/:artistId", spotify.albums);
+router.get("/tracks/:albumId", spotify.tracks);
 
 module.exports = router;
--- controllers/spotify.controller.js
+++ controllers/spotify.controller.js
@@ -10,7 +10,6 @@ module.exports.search = (req, res) => {
     spotifyApi
         .searchArtists(req.query.search)
         .then((data) => {
-            console.log("The received data from the API: ", data.body.artists.items)
             //return res.json(data) --> podemos usar para pintar el json resultante en el navegador, corta el flujo del código por hacer return
             //console.log("The received data from the API: ", data.body); --> lo mismo que la línea anterior pero nos lo saca por consola sin cortar el flujo
             res.render("artist-search-results", { artists: data.body.artists.items })
@@ -25,6 +24,18 @@ module.exports.albums = (req, res) => {
         .getArtistAlbums(req.params.artistId)
         .then((data) => {
             res.render("albums", { albums: data.body.items })
+        })
+        .catch((err) =>
+            console.log("The error while searching artists occurred: ", err)
+        );
+}
+
+module.exports.tracks = (req, res) => {
+    spotifyApi
+        .getAlbumTracks(req.params.albumId)
+        .then((data) => {
+
+            res.render("tracks", { tracks: data.body.items })
         })
         .catch((err) =>
             console.log("The error while searching artists occurred: ", err)
